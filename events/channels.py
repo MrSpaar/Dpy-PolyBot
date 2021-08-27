8cfcb76f648a78fbe856c3adae32c6f601b684a8
MrSpaar <julienpistre.cagnes@gmail.com>
2021-08-27 22:54:11 +0200

    Minor fix

--- events/channels.py
+++ events/channels.py
@@ -8,29 +8,28 @@ class Channels(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        name = f'Salon de {member.display_name}'
-        temp = await self.bot.db.pending.find({'guild_id': member.guild.id, 'name': name})
+        entry = await self.bot.db.pending.find({'guild_id': member.guild.id, 'id': member.id})
 
-        if after.channel and 'Créer' in after.channel.name and not member.bot and not temp:
+        if after.channel and 'Créer' in after.channel.name and not member.bot and not entry:
             if cat := after.channel.category:
-                overwrites = after.channel.overwrites
-                text = await member.guild.create_text_channel(name=f'Salon-de-{member.display_name}', category=cat, overwrites=overwrites)
-                channel = await member.guild.create_voice_channel(name=name, category=cat, overwrites=overwrites)
+                text = await member.guild.create_text_channel(name=f'Salon-de-{member.display_name}', category=cat, overwrites=after.channel.overwrites)
+                channel = await member.guild.create_voice_channel(name=f'Salon de {member.display_name}', category=cat, overwrites=after.channel.overwrites)
+
                 try:
                     await member.move_to(channel)
-                    await self.bot.db.pending.insert({'type': 'channel', 'guild_id': member.guild.id, 'name': name, 'txt_id': text.id})
+                    await self.bot.db.pending.insert({'guild_id': member.guild.id, 'id': member.id, 'voc_id': channel.id, 'txt_id': text.id})
                 except:
                     await channel.delete()
                     await text.delete()
             return
 
         if before.channel:
-            chan = await self.bot.db.pending.find({'guild_id': member.guild.id, 'name': before.channel.name})
-            if chan and ((len(before.channel.members) <= 1 and member.guild.me in before.channel.members) or not len(before.channel.members)):
+            entry = await self.bot.db.pending.find({'guild_id': member.guild.id, 'voc_id': before.channel.id})
+            if entry and ((len(before.channel.members) <= 1 and member.guild.me in before.channel.members) or not len(before.channel.members)):
                 await before.channel.delete()
-                channel = get(member.guild.text_channels, id=chan['txt_id'])
+                channel = get(member.guild.text_channels, id=entry['txt_id'])
                 await channel.delete()
-                await self.bot.db.pending.delete({'guild_id': member.guild.id, 'name': before.channel.name})
+                await self.bot.db.pending.delete(entry)
 
 
 def setup(bot):
